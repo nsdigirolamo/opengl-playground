d1d041ce0f9b1bec4d21603632bf67b5aa78dc9d
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-07-08 18:44:20 -0400

    Shaders now minimally working.

--- src/solitaire-window.cpp
+++ src/solitaire-window.cpp
@@ -6,6 +6,21 @@
 constexpr unsigned int WINDOW_WIDTH = 768;
 constexpr unsigned int WINDOW_HEIGHT = 432;
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+
 void errorCallback (int error, const char* description)
 {
     std::cerr << "GLFW Error: " << description << std::endl;
@@ -13,7 +28,12 @@ void errorCallback (int error, const char* description)
     exit(-1);
 }
 
-static void keyCallback (GLFWwindow* window, int key, int scancode, int action, int mods)
+void framebufferSizeCallback (GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void keyCallback (GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
@@ -38,6 +58,8 @@ int main ()
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glfwSetKeyCallback(window, keyCallback);
 
     if (!gladLoadGLLoader((GLADloadproc)(glfwGetProcAddress)))
     {
@@ -46,10 +68,72 @@ int main ()
         exit(-1);
     }
 
-    glfwSetKeyCallback(window, keyCallback);
+    int compilerSuccess;
+    char compilerInfo [512];
+
+    const unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compilerSuccess);
+    if (!compilerSuccess) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, compilerInfo);
+        std::cerr << "Vertex Shader Compilation Failed: " << compilerInfo << std::endl;
+    }
+
+    const unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &compilerSuccess);
+    if (!compilerSuccess) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, compilerInfo);
+        std::cerr << "Fragment Shader Compilation Failed: " << compilerInfo << std::endl;
+    }
+
+    const unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetShaderiv(shaderProgram, GL_COMPILE_STATUS, &compilerSuccess);
+    if (!compilerSuccess) {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, compilerInfo);
+        std::cerr << "Shader Program Linkage Failed: " << compilerInfo << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    const float vertices [] =
+    {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int vertexArray, vertexBuffer;
+
+    glGenVertexArrays(1, &vertexArray);
+    glGenBuffers(1, &vertexBuffer);
+
+    glBindVertexArray(vertexArray);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(0));
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vertexArray);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
