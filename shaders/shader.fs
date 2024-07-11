0a1cc03d90a8209ea6ef7b99ded317be78f468e8
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-07-11 13:40:58 -0400

    Adjusted the shaders to change colors based on normals.

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -1,7 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 Norm;
+
 void main()
 {
-	FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	vec3 crossed = cross(vec3(1.0, 0.0, 0.0), Norm);
+	FragColor = vec4(length(crossed), 0.0, 0.0, 1.0);
 }
