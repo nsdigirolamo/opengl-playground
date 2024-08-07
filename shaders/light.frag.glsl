77520aa21a30d52a29377cf82673dcb1de77d1a6
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-08-06 20:18:49 -0400

    Materials now control colors

--- shaders/light.frag.glsl
+++ shaders/light.frag.glsl
@@ -1,9 +1,16 @@
 #version 330 core
 out vec4 fragmentColor;
 
-uniform vec3 lightColor;
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shine;
+};
+
+uniform Material lightMaterial;
 
 void main()
 {
-	fragmentColor = vec4(lightColor, 1.0);
+	fragmentColor = vec4(lightMaterial.specular, 1.0);
 }
--- shaders/nonlight.frag.glsl
+++ shaders/nonlight.frag.glsl
@@ -1,29 +1,34 @@
 #version 330 core
 out vec4 fragmentColor;
 
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shine;
+};
+
 in vec3 fragmentPosition;
 in vec3 surfaceNormal;
 
 uniform vec3 lightPosition;
 uniform vec3 viewPosition;
-uniform vec3 lightColor;
-uniform vec3 objectColor;
+
+uniform Material objectMaterial;
+uniform Material lightMaterial;
 
 void main()
 {
-	float ambientStrength = 0.1;
-	vec3 ambient = ambientStrength * lightColor;
+	vec3 ambient = lightMaterial.ambient * objectMaterial.ambient;
 
 	vec3 lightDirection = normalize(lightPosition - fragmentPosition);
 	float diffuseStrength = max(dot(surfaceNormal, lightDirection), 0.0);
-	vec3 diffuse = diffuseStrength * lightColor;
+	vec3 diffuse = lightMaterial.diffuse * diffuseStrength * objectMaterial.diffuse;
 
-	float specularStrength = 0.5;
 	vec3 viewDirection = normalize(viewPosition - fragmentPosition);
 	vec3 reflectDirection = reflect(-lightDirection, surfaceNormal);
-	float highlight = pow(max(dot(viewDirection, reflectDirection), 0.0), 32);
-	vec3 specular = specularStrength * highlight * lightColor;
+	float specularStrength = pow(max(dot(viewDirection, reflectDirection), 0.0), objectMaterial.shine);
+	vec3 specular = lightMaterial.specular * specularStrength * objectMaterial.specular;
 
-	vec3 result = (diffuse + ambient + specular) * objectColor;
-	fragmentColor = vec4(result, 1.0);
+	fragmentColor = vec4(ambient + diffuse + specular, 1.0);
 }
