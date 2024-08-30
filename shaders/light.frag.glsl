63cb200b0c3d53e987effdec3bae2d948615b8c7
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-08-29 23:02:06 -0400

    Edited shaders to use new texturing system.

--- shaders/light.frag.glsl
+++ shaders/light.frag.glsl
@@ -1,16 +1,15 @@
 #version 330 core
 out vec4 fragmentColor;
 
-struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float shine;
+struct Light {
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
 };
 
-uniform Material lightMaterial;
+uniform Light light;
 
 void main()
 {
-	fragmentColor = vec4(lightMaterial.specular, 1.0);
+	fragmentColor = vec4(light.specular, 1.0);
 }
