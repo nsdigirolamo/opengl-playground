fd735e376ba6a05759eb5bb378b2ebf2030a4067
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-08-30 23:31:55 -0400

    Many changes to match with refactoring.

--- src/solitaire-window.cpp
+++ src/solitaire-window.cpp
@@ -3,6 +3,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include "camera.hpp"
 #include "shader.hpp"
+#include "object.hpp"
 
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -67,44 +68,27 @@ int main ()
     Shader nonLightShader { "shaders/nonlight.vert.glsl", "shaders/nonlight.frag.glsl" };
     Shader lightShader { "shaders/light.vert.glsl", "shaders/light.frag.glsl" };
 
-    Texture diffuseMap {
-        "textures/box_texture_diffuse_map.png"
-    };
+    Model cubeModel { "models/cube.obj" };
+    Texture diffuseMap { "textures/box_texture_diffuse_map.png" };
+    Texture specularMap { "textures/box_texture_specular_map.png" };
+    Texture emissiveMap { "textures/box_texture_empty.png" };
+    Material cubeMaterial { diffuseMap.getID(), specularMap.getID(), emissiveMap.getID(), 64.0f };
+    Object cube { glm::vec3(0.0f), glm::vec3(1.0f), &cubeModel, &cubeMaterial };
 
-    Texture specularMap {
-        "textures/box_texture_specular_map.png"
-    };
-
-    Material material {
-        diffuseMap.getID(),
-        specularMap.getID(),
-        64.0f
-    };
-
-    std::cout << material.diffuse << " " << material.specular << std::endl;
-
+    Model lightModel { "models/cube.obj" };
     Light light {
+        glm::vec3(0.0f),
+        glm::vec3(0.2f),
+        &lightModel,
         glm::vec3(0.2f, 0.2f, 0.2f),
         glm::vec3(0.5f, 0.5f, 0.5f),
-        glm::vec3(1.0f, 1.0f, 1.0f)
-    };
-
-    Model objectModel {
-        "models/cube.obj",
-        glm::vec3(0.0f),
-        glm::vec3(1.0f)
-    };
-
-    Model lightModel {
-        "models/cube.obj",
-        glm::vec3(0.0f),
-        glm::vec3(0.2f)
+        glm::vec3(1.0f, 1.0f, 1.0f),
     };
 
     Camera camera;
 
     double previousTime = glfwGetTime();
-    
+
     while (!glfwWindowShouldClose(window))
     {
         double currentTime = glfwGetTime();
@@ -132,19 +116,27 @@ int main ()
         glm::mat4 modelMat = glm::mat4(1.0f);
 
         glm::mat4 translateMat = glm::mat4(1.0f);
-        translateMat = glm::translate(translateMat, objectModel.position);
+        translateMat = glm::translate(translateMat, cube.position);
 
         glm::mat4 scaleMat = glm::mat4(1.0f);
-        scaleMat = glm::scale(scaleMat, objectModel.scale);
+        scaleMat = glm::scale(scaleMat, cube.scale);
 
         nonLightShader.use();
 
-        nonLightShader.setVec3("lightPosition", lightModel.position);
+        nonLightShader.setVec3("lightPosition", light.position);
         nonLightShader.setVec3("viewPosition", camera.position);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, cube.material->diffuse);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, cube.material->specular);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, cube.material->emissive);
 
         nonLightShader.setInt("objectMaterial.diffuse", 0);
         nonLightShader.setInt("objectMaterial.specular", 1);
-        nonLightShader.setFloat("objectMaterial.shine", material.shine);
+        nonLightShader.setInt("objectMaterial.emissive", 2);
+        nonLightShader.setFloat("objectMaterial.shine", cube.material->shine);
 
         nonLightShader.setVec3("light.ambient", light.ambient);
         nonLightShader.setVec3("light.diffuse", light.diffuse);
@@ -156,25 +148,20 @@ int main ()
         nonLightShader.setMat4("translate", translateMat);
         nonLightShader.setMat4("scale", scaleMat);
 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, material.diffuse);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, material.specular);
-
-        objectModel.bindVertexBuffer();
-        objectModel.bindVertexArray();
-        objectModel.drawVertexArray();
+        cube.model->bindVertexBuffer();
+        cube.model->bindVertexArray();
+        cube.model->drawVertexArray();
 
         lightShader.use();
 
-        lightModel.position.x = 5 * sin(currentTime);
-        lightModel.position.z = 5 * cos(currentTime);
+        light.position.x = 5 * sin(currentTime);
+        light.position.z = 5 * cos(currentTime);
 
         translateMat = glm::mat4(1.0f);
-        translateMat = glm::translate(translateMat, lightModel.position);
+        translateMat = glm::translate(translateMat, light.position);
 
         scaleMat = glm::mat4(1.0f);
-        scaleMat = glm::scale(scaleMat, lightModel.scale);
+        scaleMat = glm::scale(scaleMat, light.scale);
 
         lightShader.setMat4("projection", projectionMat);
         lightShader.setMat4("view", viewMat);
